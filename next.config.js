cb44d4d48577cc594e49bd817d518f814d0dcd36
Himanshu <himanshushettykt03@gmail.com>
2025-11-15 00:17:14 +0530

    minor fix

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  skipTrailingSlashRedirect: true,
-  distDir: 'dist',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH ? `${process.env.NEXT_PUBLIC_BASE_PATH}/` : '',
   images: {
     unoptimized: true,
     domains: ["localhost"],
